b472e370009d2f99288f839f2b2db9d745a76022
Maciej Łukasiewicz <s197865@student.pg.edu.pl>
2026-01-27 19:54:09 +0100

    removing pointless files

--- python_app/check_db_robust.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import json
-conn = sqlite3.connect("jumps_data.db")
-c = conn.cursor()
-c.execute("PRAGMA table_info(jumps)")
-cols = c.fetchall()
-with open("cols_list.json", "w") as f:
-    json.dump(cols, f)
-conn.close()

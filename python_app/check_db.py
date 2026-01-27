29c75cdea1f1872b72521505ba3ce449ff0dd645
Maciej Łukasiewicz <s197865@student.pg.edu.pl>
2026-01-27 19:54:09 +0100

    removing pointless files

--- python_app/check_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("jumps_data.db")
-c = conn.cursor()
-c.execute("PRAGMA table_info(jumps)")
-cols = c.fetchall()
-for col in cols:
-    print(col)
-conn.close()
